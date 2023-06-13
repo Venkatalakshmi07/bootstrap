f84aa26324c66280e6c1e5f6652b89f808f8bf3d
tripurna <tripurnatripurna@gmail.com>
2023-06-13 12:19:23 +0530

    hii

---
+++ main.js
@@ -0,0 +1,80 @@
+//      var x=10;
+//       var x=20;
+//   document.write(x);
+//    const firstName="web";
+//     const lastName="development";
+//     console.log(firstName +" " + lastName);
+    // window.alert("submit succefully");
+    // document.getElementById("demo").innerHTML = firstName + " " + lastName;
+    // const firstName="tripurna";
+    // const lastName="bantu";
+    // window.prompt( lastName);
+    // window.confirm(lastName);
+    // operaters
+    // arithematic operators
+    // var x=2;
+    // var y=3;
+    // var z=x+y;
+    // var q=x-y;
+    // var p=x*y;
+    // var r=x/y;
+    // var t=x**y;
+    // var n=x%y;
+    // console.log(z);
+    // console.log(q);
+    // console.log(p);
+    // console.log(r);
+    // console.log(t);
+    // console.log(n);
+    // // assignment operators
+    // var x=2;
+    // x +=10;
+    // y -=10;
+    // p *=10;
+    // q /=10;
+    // t %=10;
+    // r**=10;
+    // console.log(x);
+    // console.log(y);
+    // console.log(p);
+    // console.log(q);
+    // console.log(t);
+    // console.log(r);
+    // comparison operator
+    const p="10";
+    const q="10";
+    console.log(p>q);
+    console.log(p<q);
+    console.log(p>=q);
+    console.log(p<=q);
+    console.log(p==q);
+    console.log(p === q);
+    // logical operator
+    // and operator
+    let x=20;
+    let y=30;
+    console.log(x>y&&x<y);
+    console.log(x<y&&x>y);
+    console.log(x<y&&x<y);
+    console.log(x>y&&x>y);
+    // or operator
+    const c=40;
+    const d=50;
+    console.log(c>d||c<d);
+    console.log(c<d||c>d);
+    console.log(c<d||c<d);
+    console.log(c>d||c>d);
+    // not operator
+    var t=10;
+    var s=20;
+    console.log(!(t<s));
+    // type operator
+    let name="web";
+    let n=true;
+    console.log(typeof(n));
+    // ternary
+    console.log((x>y ? "tripurna" : "srinu"));
+
+
+    
+
